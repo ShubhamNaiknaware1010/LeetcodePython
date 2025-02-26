3be17ff4879ca46c41177375072bad8c2d66d7cf
Shubham Naiknaware <shubhamnaiknaware1010@gmail.com>
2025-02-26 23:06:07 +0530

    ADDED EVEN ODD USING OR

--- new.py
+++ new.py
@@ -1,8 +1,16 @@
+# FIXME : Check if the number is odd or even
 num = int(input("Enter a number: "))
-
 ans = num & 1
 if ans == 1:
     print("Odd")
 else:   
     print("Even")
-    
+
+
+# using OR
+
+ans = num | 1
+if ans == num:
+    print("Odd")
+else:   
+    print("Even")
